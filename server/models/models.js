a0ac426edb6c5725b5d5fa232e70e3e17f4156fc
Tyler Haigh <Tyler_Haigh@PacificNational.com.au>
2017-12-09 14:03:37 +1100

    Setup Mongoose

---
+++ server/models/models.js
@@ -0,0 +1,7 @@
+const Todo = require('./todo-model').Todo;
+const User = require('./user-model').User;
+
+module.exports = {
+    Todo,
+    User
+};
---
+++ server/server.js
@@ -0,0 +1,27 @@
+
+const {mongoose} = require('./db/mongoose');
+const { Todo, User } = require('./models/models');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({text: req.body.text});
+
+    todo.save().then( (doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    })
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
