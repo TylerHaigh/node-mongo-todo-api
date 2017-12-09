f90f5377f281a51c0b80b0d1ad6896ea06487bbe
Tyler Haigh <Tyler_Haigh@PacificNational.com.au>
2017-12-09 20:39:53 +1100

    Add Login endpoint

--- server/server.js
+++ server/server.js
@@ -125,7 +125,30 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
-})
+});
+
+
+
+
+app.post('/users/login', (req, res) => {
+
+    var body = _.pick(req.body, ['email', 'password']);
+    //res.send(body);
+
+    User.findByCredentials(body.email, body.password).then( (user) => {
+        user.generateAuthToken().then( (token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch( (err) => {
+        res.status(400).send();
+    });
+
+});
+
+
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
